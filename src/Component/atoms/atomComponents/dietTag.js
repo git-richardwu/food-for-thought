0ff1027d5283a838b0b84ff0080e3866504bd3c0
Andrew Jank <36163464+pockets3407@users.noreply.github.com>
2021-04-25 13:55:54 -0400

    #139 #143 fixed contrast on profile page diet tags

--- src/Component/atoms/atomComponents/dietTag.js
+++ src/Component/atoms/atomComponents/dietTag.js
@@ -6,10 +6,10 @@ function DietTag({ dietTag }) {
     <div className={styles.dietTagContainer}>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
   <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"/>
-    {dietTag !== "" &&       
-        <button className={styles.dietTageStyle}>
-            <p className={styles.buttonTitle}>{dietTag}</p>
-        </button>
+    {dietTag !== "" &&    
+        <div className={styles.profileDietTag}>
+            {dietTag}
+        </div>   
     }
 
     </div>
